f63220c9b2502cac806a6b394b2fec909af0aefc
Chanut <chanut.wongtripipat@gmail.com>
2022-09-04 21:12:53 +0700

    update frontend code

--- server/app.js
+++ server/app.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const users = require('./db.json')
+const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
+
+// app.use(express.json)
+// app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json)
+app.use(bodyParser.urlencoded({ extended: true}))
 
 app.get('/', (req, res) => {
     res.send('hello world')
@@ -11,6 +17,17 @@ app.get('/users', (req, res) => {
     res.json(users)
 })
 
+app.get('/users/:id', (req, res) => {
+    res.json(users.find(user => user.id === Number(req.params.id)))
+})
+
+app.post('/users', (req, res) => {
+    users.push(req.body)
+    let json = req.body
+    res.send(`Add new user '${json.username}' completed.`)
+})
+
 app.listen(port, () => {
     console.log(`Starting node add port : ${port}`);
 })
+
