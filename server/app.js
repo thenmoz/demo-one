53136e5b1dc0e80cef58ecd0075ba4aad8258323
chanut <chanut.wongtripipat@gmail.com>
2022-10-08 19:19:56 +0700

    update be

--- server/app.js
+++ server/app.js
@@ -4,9 +4,7 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use(express.json)
-// app.use(express.urlencoded({ extended: true }))
-app.use(bodyParser.json)
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}))
 
 app.get('/', (req, res) => {
